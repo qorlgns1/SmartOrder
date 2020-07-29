e22e0642f4af56331420793f582028523279a580
marco <rlgns1129@naver.com>
2020-07-29 20:15:46 +0900

    Initial commit

---
+++ app/src/main/java/naver/rlgns1129/smartorder/MainActivity.java
@@ -0,0 +1,14 @@
+package naver.rlgns1129.smartorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
