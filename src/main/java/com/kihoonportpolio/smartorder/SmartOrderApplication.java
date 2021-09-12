461f09486e26f4cd0b4e5b0b0a9a3f4240fcfe1a
qorlgns1 <rlgns0610@gmail.com>
2021-09-12 09:56:56 +0900

    스프링부트 시작하기

---
+++ src/main/java/com/kihoonportpolio/smartorder/SmartOrderApplication.java
@@ -0,0 +1,13 @@
+package com.kihoonportpolio.smartorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kihoonportpolio/smartorder/SmartOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kihoonportpolio.smartorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
